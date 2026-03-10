9195bef1eed16ab51ae7eed08906f5b34c9cc019
Bhavyasri104 <cse240001049@iiti.ac.in>
2026-03-10 17:35:02 +0530

    Update part1_legacy_AB.py

--- codes/part1_legacy_AB.py
+++ codes/part1_legacy_AB.py
@@ -6,7 +6,6 @@ rpc_password = "labpass123"
 rpc_port = 18443
 
 rpc = AuthServiceProxy(f"http://{rpc_user}:{rpc_password}@127.0.0.1:{rpc_port}")
-
 print("Connected to Bitcoin Core")
 
 wallet_name = "labwallet"
@@ -15,18 +14,18 @@ try:
     rpc.createwallet(wallet_name)
     print("Wallet created")
 except JSONRPCException:
-    print("Wallet may already exist, loading it...")
+    print("Wallet already exists, loading...")
     try:
         rpc.loadwallet(wallet_name)
-        print("Wallet loaded")
     except:
         pass
+
 
 rpc = AuthServiceProxy(
     f"http://{rpc_user}:{rpc_password}@127.0.0.1:{rpc_port}/wallet/{wallet_name}"
 )
 
-# Generate addresses
+
 A = rpc.getnewaddress("A", "legacy")
 B = rpc.getnewaddress("B", "legacy")
 C = rpc.getnewaddress("C", "legacy")
@@ -35,39 +34,35 @@ print("Address A:", A)
 print("Address B:", B)
 print("Address C:", C)
 
-# Mine blocks
+
 rpc.generatetoaddress(101, A)
 print("Mined 101 blocks")
 
-# Send BTC to A
+
 fund_txid = rpc.sendtoaddress(A, Decimal("1.0"))
-print("Funding Transaction ID:", fund_txid)
+print("Funding TXID:", fund_txid)
 
 rpc.generatetoaddress(1, A)
 print("Block mined to confirm funding")
 
-# Get UTXO
+
 utxos = rpc.listunspent(1, 9999999, [A])
 
 if not utxos:
-    print("No UTXO found")
+    print("No UTXO found for address A")
     exit()
 
 utxo = utxos[0]
-print("UTXO selected:", utxo)
+print("Selected UTXO:", utxo)
 
 inputs = [{
     "txid": utxo["txid"],
     "vout": utxo["vout"]
 }]
 
-# Fee
 fee = Decimal("0.0001")
 
-# Send half of available balance
 send_amount = utxo["amount"] / 2
-
-# Change calculation
 change_amount = utxo["amount"] - send_amount - fee
 
 outputs = {
@@ -75,34 +70,46 @@ outputs = {
     A: change_amount
 }
 
-# Create raw transaction
+
 raw_tx = rpc.createrawtransaction(inputs, outputs)
 
 print("\nRaw Transaction:")
 print(raw_tx)
 
-# Sign transaction
+
+decoded_raw = rpc.decoderawtransaction(raw_tx)
+
+print("\nDecoded Raw Transaction:")
+print(decoded_raw)
+
+
 signed_tx = rpc.signrawtransactionwithwallet(raw_tx)
 
 if not signed_tx["complete"]:
-    print("Transaction signing incomplete")
+    print("Signing incomplete")
     exit()
 
-print("\nSigned Transaction:")
+print("\nSigned Transaction HEX:")
 print(signed_tx["hex"])
 
-# Broadcast
+
 txid = rpc.sendrawtransaction(signed_tx["hex"])
 
 print("\nA -> B Transaction ID:", txid)
 
-# Confirm transaction
+
 rpc.generatetoaddress(1, A)
 
 print("Block mined to confirm A -> B")
 
-# Decode transaction
-decoded_tx = rpc.gettransaction(txid)
 
-print("\nDecoded Transaction:")
+decoded_tx = rpc.decoderawtransaction(signed_tx["hex"])
+
+print("\nDecoded Final Transaction:")
 print(decoded_tx)
+
+print("\nLocking Script (scriptPubKey):")
+print(decoded_tx["vout"][0]["scriptPubKey"])
+
+print("\nUnlocking Script (scriptSig):")
+print(decoded_tx["vin"][0]["scriptSig"])
